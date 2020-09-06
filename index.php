ec3d77a6911973e9b30fcc4bd051fe4aebb0a6b3
ahspsh <59199884+ahspsh@users.noreply.github.com>
2020-09-06 10:08:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php 
 require ($_SERVER['DOCUMENT_ROOT'].'/c/c.php');
 
-
-
+$a['[t]']="NOSQL-PHP";
+$a['[d']="一个不需要数据库的php";
+$a['[k]']="无数据库";
+$a['[c]']="正文";
 echo th(dk($_SERVER['DOCUMENT_ROOT'].'/m/index.php')$a);
 ?>
