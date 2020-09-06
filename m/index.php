7daab462fa0da3e98b72d06369644cdb8049c4cf
ahspsh <59199884+ahspsh@users.noreply.github.com>
2020-09-06 10:04:51 +0800

    Create index.php

---
+++ m/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+	<link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css">  
+	<script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
+	<script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<html style="padding: 0px; margin: 0px; border: 0px; width:100%; height:100%;">
+<head>
+<meta name="renderer" content="webkit">
+<meta charset="utf-8"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta http-equiv="Cache-Control" content="no-transform">
+<meta http-equiv="Cache-Control" content="no-siteapp">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+<title>[t]</title>
+<meta name="description" content="[d]" />
+<meta name="keywords" content="[k]" />
+<body style="padding: 0px; margin: 0px; border: 0px; width:100%; height:100%;">
+[c]
+</body>
+</html>
